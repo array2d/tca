77a7ad62792c8b5d2b8cf2d11b5523283feafd1b
miaobyte <734991033@qq.com>
2026-06-25 12:04:33 +0800

    db: 修复数据库连接优先级, mysql/sqlite成功后提前返回

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -9,7 +9,13 @@ var db *gorm.DB
 
 func Getdb() (d *gorm.DB, err error) {
 	_mysql()
+	if db != nil {
+		return db, nil
+	}
 	_sqlite()
+	if db != nil {
+		return db, nil
+	}
 	_postgres()
 	if db != nil {
 		return db, nil
